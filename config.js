db84e7cebf168be6ce7f966003ac0661196b18a8
Bill Frye <3730304+billfrye@users.noreply.github.com>
2018-11-05 15:06:23 -0600

    initial setup

---
+++ config.js
@@ -0,0 +1,6 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+
+const Big_Json ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+const Small_Json = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
